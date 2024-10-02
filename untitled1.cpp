7262df76bcc248e7edae4d770b80d87b880ca779
hzx-dev <370865552@qq.com>
2024-10-02 19:47:01 +0800

    乱搞的
    Yes, 就是乱搞的

--- untitled1.cpp
+++ untitled1.cpp
@@ -1,66 +1,16 @@
 #include <iostream>
-#include <string>
+//#include <system>
 
 using namespace std;
 
 int main() {
-	cout << "Cout example:" << endl;
-	cout << "Hello World!" << endl;
-	
-	cout << "String example:" << endl;
-	string name;
-	cout << "Hi, what's your name?" << endl << ">>> ";
-	cin >> name;
-	cout << "Hi, " << name << "!" << endl;
-
-	cout << "Cin example:" << endl;
-	cout << "1 + 1 = ";
-	int res;
-	cin >> res;
-	
-	if(res == 2){
-		cout << "You're right!" << endl;
-	}else{
-		cout << "You're wrong!" << endl;
-	}
-	
-	cout << "While example:" << endl;
-	
-	res = -1;
-	
-	while(res != 2){
-		res = -1;
-		cout << "1 + 1 = ";
-		cin >> res;
-		if(res == 2){
-			cout << "You're right!" << endl;
-		}else
-		{
-			cout << "You're wrong!" << endl;
-		}
-	}
-	
-	cout << "Do...while example:" << endl;
-	
-	do{
-		cout << "1 + 1 = ";
-		cin >> res;
-		if(res == 2){
-			cout << "You're right!" << endl;
-		}else{
-			cout << "You're wrong!" << endl;
-		}
-	}while(res != 2);
-	
-	cout << "For example:" << endl;
-	
-	for(int i=0;i<=10;i++){
-		cout << i << endl;
-	}
-	
-	cout << "Pointer example:" << endl;
-	
-
-	return 0;
+    while(1){
+        cout << "What is your name? Enter:_____\b\b\b\b\b";
+        char tmp;
+        cin >> tmp;
+        cout << "Your name is " << tmp << endl;
+//        sleep(1);
+    }
+    return 0;
 }
 
